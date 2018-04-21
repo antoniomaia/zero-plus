1df54db60e104118af32cda1143384e60c0899a1
am <antoniobcmaia@gmail.com>
2018-04-21 21:23:02 +0100

    Add csvtojson module

--- src/components/data/DataInfo.js
+++ src/components/data/DataInfo.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const DataInfo = (props) => {
-    return (
-        <h1>{props.entries.length}</h1>
-    );
-}
+const DataInfo = props => {
+  const entries = JSON.parse(props.entries);
+  const first = entries[0].Date;
+  const last = entries[entries.length-1].Date;
+  const fastTotalHours = entries.reduce(
+      (total, currentValue) => total + Number.parseInt(currentValue.Hours, 10), 0
+  );
+
+  return (
+    <div>
+      <p>First: {first}</p>
+      <p>Last:{last}</p>
+      <p>Total: {entries.length}</p>
+      <p>Total Hours: {fastTotalHours}</p>
+      <p>Average Hours: {fastTotalHours/entries.length}</p>
+    </div>
+  );
+};
 
 export default DataInfo;
